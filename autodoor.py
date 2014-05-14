ede2db50b57f218128d028777ce83e3097d0b07f
Sam Quinn <quinnsam1@gmail.com>
2014-05-13 23:23:05 -0700

    Got the basic infinite loop running and printing when the system will lock or unlock the door.

--- autodoor.py
+++ autodoor.py
@@ -7,37 +7,34 @@
 
 from threading import Thread
 import subprocess
-from Queue import Queue
 
-num_threads = 4
-queue = Queue()
-ips = ["10.0.0.1", "10.0.0.9", "10.0.0.69"]
+ips = ["10.0.0.1", "10.0.0.9", "10.0.0.113", "10.0.0.111"]
+connected = []
+devices = 0
+lock_status = 0
 
-#wraps system ping command
-def pinger(i, q):
-    """Pings subnet"""
-    while True:
-        ip = q.get()
-        print "Thread %s: Pinging %s" % (i, ip)
-        ret = subprocess.call("ping -c 1 %s" % ip,
-            shell=True,
-            stdout=open('/dev/null', 'w'),
-            stderr=subprocess.STDOUT)
+while 1:
+
+    for ip in ips:
+        ret = subprocess.call("ping -c 1 -w 1 -n %s" % ip,
+                shell=True,
+                stdout=open('/dev/null', 'w'),
+                stderr=subprocess.STDOUT)
+
         if ret == 0:
-            print "%s: is alive" % ip
-        else:
-            print "%s: did not respond" % ip
-        q.task_done()
+            if ip in connected:
+                continue
+            else:
+                ++devices
+                connected.append(ip)
+                print "\n\n UNLOCKING \n\n"
+                lock_status = 0
 
-#Spawn thread pool
-for i in range(num_threads):
-    worker = Thread(target=pinger, args=(i, queue))
-    worker.setDaemon(True)
-    worker.start()
+        else :
+            if ip in connected:
+                connected.remove(ip)
+            if (len(connected) == 2 and lock_status == 0):
+                print "\n\n LOCKING \n\n"
+                lock_status = 1
 
-#Place work in queue
-for ip in ips:
-    queue.put(ip)
-
-#Wait until worker threads are done to exit
-queue.join()
+    print '%s' % ', '.join(map(str, connected))
