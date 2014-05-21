19cbb8c6adc3b00a38b61ed98a0d8de666674ade
Sam Quinn <quinnsam1@gmial.com>
2014-05-20 22:04:00 -0700

    Updated the echo server and client.

--- echo_client.py
+++ echo_client.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-""" 
-A simple echo client 
-""" 
+"""
+A simple echo client
+"""
 
-import socket 
+import socket
 
-host = 'localhost' 
-port = 50000 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host,port)) 
-s.send('Hello, world') 
-data = s.recv(size) 
-s.close() 
+host = 'mobkilla.no-ip.biz'
+port = 55555
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+s.send('Hello, world')
+data = s.recv(size)
+s.close()
 print 'Received:', data
--- echo_server.py
+++ echo_server.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-""" 
-A simple echo server 
-""" 
+"""
+A simple echo server
+"""
 
-import socket 
+import socket
 
-host = '' 
-port = 50000 
-backlog = 5 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.bind((host,port)) 
-s.listen(backlog) 
+host = ''
+port = 55555
+backlog = 5
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
 while 1:
-    client, address = s.accept() 
-    data = client.recv(size) 
+    client, address = s.accept()
+    data = client.recv(size)
+    print "data:", data
     if data:
-        client.send(data) 
+        client.send(data)
     client.close()
