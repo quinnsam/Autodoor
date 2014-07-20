e637dd5c06f1bbbc2fb4927a72eb8da56fb2c21e
Sam <quinnsam1@gmail.com>
2014-07-20 11:26:33 -0700

    Light now turns on when the door is unlocked matically.

--- Serial_Prox/Serial_Prox.ino
+++ Serial_Prox/Serial_Prox.ino
@@ -74,7 +74,7 @@ void setup()
     WriteByte(sensorAddr, 0x3, 0xFE);
 
 	// Calibrates the definitions of the potentiometer values
-    calibrate();
+    // calibrate();
 
 }
 
@@ -105,7 +105,7 @@ void loop() {
                 }
             } else {
                 calibrate();
-            }
+           }
         } else {
             Serial.print("ERROR: Unreconnized command: ");
             char out = input;
